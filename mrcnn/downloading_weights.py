a9773a24c8de6fc034e322385ea82839df9b21c8
Kumar Ashutosh <kumarashutosh.ee@gmail.com>
2018-06-01 12:46:31 +0800

    changes added to base code

---
+++ mrcnn/downloading_weights.py
@@ -0,0 +1,27 @@
+import os
+import random
+import datetime
+import re
+import math
+import logging
+from collections import OrderedDict
+import multiprocessing
+import numpy as np
+import skimage.transform
+import tensorflow as tf
+import keras
+import keras.backend as K
+import keras.layers as KL
+import keras.engine as KE
+import keras.models as KM
+
+from mrcnn import utils
+
+from keras.utils.data_utils import get_file
+TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/'\
+                                 'releases/download/v0.2/'\
+                                 'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                                TF_WEIGHTS_PATH_NO_TOP,
+                                cache_subdir='models',
+                                md5_hash='a268eb855778b3df3c7506639542a6af')
